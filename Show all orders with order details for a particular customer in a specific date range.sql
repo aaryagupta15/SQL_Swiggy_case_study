c22304ca43bacc70f09216e2216c2291c233ede5
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-06 03:47:24 +0530

    Update Show all orders with order details for a particular customer in a specific date range.sql

--- Show all orders with order details for a particular customer in a specific date range.sql
+++ Show all orders with order details for a particular customer in a specific date range.sql
@@ -1,7 +1,4 @@
-SELECT o.order_id, o.date, r.r_name, f.f_name, od.f_id
-FROM orders o
-JOIN order_details od ON o.order_id = od.order_id
-JOIN food f ON od.f_id = f.f_id
-JOIN restaurant r ON o.r_id = r.r_id
-WHERE o.user_id = 1 
-AND o.date BETWEEN '2022-06-01' AND '2022-06-30';
+SELECT *
+FROM orders
+WHERE user_id = 1 
+AND date BETWEEN '2022-05-01' AND '2022-07-01';
