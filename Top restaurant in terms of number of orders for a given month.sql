6c6bc5336cb1033c559188283fe7aa22f9c56da2
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-06 03:48:22 +0530

    Update Top restaurant in terms of number of orders for a given month.sql

--- Top restaurant in terms of number of orders for a given month.sql
+++ Top restaurant in terms of number of orders for a given month.sql
@@ -1,7 +1,7 @@
-SELECT r_name, COUNT(order_id) AS total_orders
-FROM restaurant r
-JOIN orders o ON r.r_id = o.r_id
-WHERE MONTH(o.date) = 6 
+SELECT r_name, COUNT(*) AS total_orders
+FROM orders
+JOIN restaurant ON orders.r_id = restaurant.r_id
+WHERE MONTH(date) = 6
 GROUP BY r_name
 ORDER BY total_orders DESC
 LIMIT 1;
