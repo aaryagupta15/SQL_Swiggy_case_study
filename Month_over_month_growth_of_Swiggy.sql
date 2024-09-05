fa7a3b1794ac966eeb31ec7cf855406e39395e8e
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-06 03:49:38 +0530

    Update and rename Month_over_month_growth_of_Swiggy.sql to Find Restaurants with Maximum Repeated Customers

--- Month_over_month_growth_of_Swiggy.sql
+++
@@ -1,4 +0,0 @@
-SELECT YEAR(date) AS year, MONTH(date) AS month, SUM(amount) AS total_revenue,
-       (SUM(amount) - LAG(SUM(amount)) OVER (ORDER BY YEAR(date), MONTH(date))) / LAG(SUM(amount)) OVER (ORDER BY YEAR(date), MONTH(date)) * 100 AS revenue_growth_percentage
-FROM orders
-GROUP BY YEAR(date), MONTH(date);
