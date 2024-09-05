005f80ddfcdced20a0bf289a9a10e348c0e38272
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-06 03:48:02 +0530

    Update Restaurants with monthly sales greater than a specified amount.sql

--- Restaurants with monthly sales greater than a specified amount.sql
+++ Restaurants with monthly sales greater than a specified amount.sql
@@ -1,6 +1,6 @@
-SELECT r_name, SUM(amount) AS total_sales
-FROM restaurant r
-JOIN orders o ON r.r_id = o.r_id
-WHERE MONTH(o.date) = 7 -- Specify the month
+SELECT r_name, SUM(amount) AS monthly_sales
+FROM orders
+JOIN restaurant ON orders.r_id = restaurant.r_id
+WHERE MONTH(date) = 7 
 GROUP BY r_name
-HAVING total_sales > 500; -- Replace 500 with your specified amount
+HAVING monthly_sales > 5000; 
