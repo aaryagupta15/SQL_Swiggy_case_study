dabde14d4a50f2f60be7a1d3f1ec091e620fe42a
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-06 03:52:43 +0530

    Update Month-over-month revenue growth for a restaurant.sql

--- Month-over-month revenue growth for a restaurant.sql
+++ Month-over-month revenue growth for a restaurant.sql
@@ -1,7 +1,14 @@
-SELECT
-    MONTH(date) AS month,
-    SUM(amount) AS monthly_revenue,
-    (SUM(amount) - LAG(SUM(amount)) OVER (ORDER BY MONTH(date))) / LAG(SUM(amount)) OVER (ORDER BY MONTH(date)) * 100 AS revenue_growth_percentage
-FROM orders
-GROUP BY month
-ORDER BY month;
+SELECT 
+    restaurant.r_name,
+    MONTH(orders.date) AS month,
+    SUM(orders.amount) AS monthly_revenue,
+    (SUM(orders.amount) - LAG(SUM(orders.amount)) OVER (PARTITION BY restaurant.r_id ORDER BY MONTH(orders.date))) / 
+    LAG(SUM(orders.amount)) OVER (PARTITION BY restaurant.r_id ORDER BY MONTH(orders.date)) * 100 AS revenue_growth_percentage
+FROM 
+    orders
+JOIN 
+    restaurant ON orders.r_id = restaurant.r_id
+GROUP BY 
+    restaurant.r_name, month
+ORDER BY 
+    restaurant.r_name, month;
