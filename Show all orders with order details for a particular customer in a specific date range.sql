7e2dbd4d795b5638e6da2eee47fcd697af2201b2
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-14 04:19:17 +0530

    Update Show all orders with order details for a particular customer in a specific date range.sql

--- Show all orders with order details for a particular customer in a specific date range.sql
+++ Show all orders with order details for a particular customer in a specific date range.sql
@@ -1,4 +1,9 @@
-SELECT *
-FROM orders
-WHERE user_id = 1 
-AND date BETWEEN '2022-05-01' AND '2022-07-01';
+SELECT o.user_id, o.order_id, r.r_name,od.f_id, f.f_name FROM zomato.orders o
+JOIN zomato.restaurants r
+on o.r_id = r.r_id
+JOIN zomato.order_details od
+ON o.order_id = od.order_id
+JOIN zomato.food f
+ON f.f_id = od.f_id 
+WHERE user_id = (SELECT user_id FROM zomato.users WHERE name LIKE 'Ankit')
+AND (date > '2022-05-30' AND date <= '2022-06-30')
