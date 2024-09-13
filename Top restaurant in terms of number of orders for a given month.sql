b4e288563934ec6fd2de3cdfa2e6f3891b5b5544
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-14 03:55:37 +0530

    Update Top restaurant in terms of number of orders for a given month.sql

--- Top restaurant in terms of number of orders for a given month.sql
+++ Top restaurant in terms of number of orders for a given month.sql
@@ -1,7 +1,4 @@
-SELECT r_name, COUNT(*) AS total_orders
-FROM orders
-JOIN restaurant ON orders.r_id = restaurant.r_id
-WHERE MONTH(date) = 6
-GROUP BY r_name
-ORDER BY total_orders DESC
-LIMIT 1;
+SELECT r_name, count(*) FROM swiggy.orders o JOIN swiggy.restaurants r ON r.r_id = o.r_id 
+WHERE monthname(date) = 'June'
+GROUP BY r_name 
+ORDER BY COUNT(*) DESC LIMIT 1
