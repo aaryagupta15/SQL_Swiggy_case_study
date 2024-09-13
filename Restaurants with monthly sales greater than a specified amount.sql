b980d8756089a63770c98e44d9f076b9f892fe75
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-14 04:02:33 +0530

    Update Restaurants with monthly sales greater than a specified amount.sql

--- Restaurants with monthly sales greater than a specified amount.sql
+++ Restaurants with monthly sales greater than a specified amount.sql
@@ -1,6 +1,6 @@
-SELECT r_name, SUM(amount) AS monthly_sales
-FROM orders
-JOIN restaurant ON orders.r_id = restaurant.r_id
-WHERE MONTH(date) = 7 
+SELECT r_name, SUM(amount) AS 'monthy sale' FROM zomato.orders o
+JOIN zomato.restaurants r 
+ON o.r_id = r.r_id
+WHERE MONTHNAME(date) = 'June' 
 GROUP BY r_name
-HAVING monthly_sales > 5000; 
+HAVING SUM(amount)>500
