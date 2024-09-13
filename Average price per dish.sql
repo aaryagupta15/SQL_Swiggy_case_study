62207a2ca5de6be0004d824724cbe058cc1c8853
Aarya Gupta <g.aarya1504@gmail.com>
2024-09-14 03:41:21 +0530

    Update Average price per dish.sql

--- Average price per dish.sql
+++ Average price per dish.sql
@@ -1,3 +1,4 @@
-SELECT f.f_name, avg(price) as'Avg Price' FROM swiggy.menu m 
-JOIN swiggy.food f ON m.f_id = f.f_id 
-GROUP BY m.f_id
+SELECT f.f_name, AVG(m.price) AS "Avg Price" 
+FROM zomato.menu m 
+JOIN zomato.food f ON m.f_id = f.f_id 
+GROUP BY f.f_name;
