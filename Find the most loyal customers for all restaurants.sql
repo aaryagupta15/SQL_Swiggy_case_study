7e8a0f2f3bd02254304e9df5436c903568845692
Aarya Gupta <g.aarya1504@gmail.com>
2024-11-07 06:04:56 +0530

    Update Find the most loyal customers for all restaurants.sql

--- Find the most loyal customers for all restaurants.sql
+++ Find the most loyal customers for all restaurants.sql
@@ -1,6 +1,5 @@
-SELECT r_name, users.name, COUNT(orders.user_id) AS total_orders
+SELECT r_name, users.name, COUNT(orders.user_id) AS 'total_orders'
 FROM orders
 JOIN restaurant ON orders.r_id = restaurant.r_id
 JOIN users ON orders.user_id = users.user_id
-GROUP BY r_name, users.name
-ORDER BY total_orders DESC;
+GROUP BY r_name, users.name HAVING COUNT(orders.user_id)>1
